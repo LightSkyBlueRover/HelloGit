9b351ba0ac61468eb042fbc7060124dab5efbd16
Robert <robertpschmidt42@gmail.com>
2025-08-28 15:49:36 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Robert Schmidt 
+ * Date: 2025/08/28
+ * 
+ * Learning GitHub with a simple print name and major function
+ * 
+ * @author Robert Schmidt
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		
+		System.out.println("My name is Robert");
+		System.out.println("My major is Computer Engineering");
+		
+	}
+}
