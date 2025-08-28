86e7e69826dab39e879db45541366c6a97bb7d81
Robert <robertpschmidt42@gmail.com>
2025-08-28 15:54:16 -0500

    Added in major (but rather just ! because I already had the print line)

--- src/Hello.java
+++ src/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
 	public static void main(String args[]) {
 		
 		System.out.println("My name is Robert");
-		System.out.println("My major is Computer Engineering");
+		System.out.println("My major is Computer Engineering!!!!!");
 		
 	}
 }
